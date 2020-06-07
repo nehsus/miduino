5b45862ad6ad205aff164256d374759c20c1240b
nehsus <sushenk@statosphere.io>
2020-06-07 19:29:31 +0530

    int16_t changes:

--- run.ino
+++ run.ino
@@ -2,45 +2,20 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
 #include "math.h"
-//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
-#include <UTFT.h>
-#include <UTouch.h>
-
-#define pad_bg 0,125,0 
-#define reticle_color 123,125,0 //R,G,B
-
-#define pad_topX 10
-#define pad_topY 10
-#define pad_bottomX 229
-#define pad_bottomY 229
+#include "UTFT.h"
+#include "UTouch.h"
 
 #define ILI9341_BLACK       0x0000  ///<   0,   0,   0
-#define ILI9341_NAVY        0x000F  ///<   0,   0, 123
 #define ILI9341_DARKGREEN   0x03E0  ///<   0, 125,   0
-#define ILI9341_DARKCYAN    0x03EF  ///<   0, 125, 123
-#define ILI9341_MAROON      0x7800  ///< 123,   0,   0
-#define ILI9341_PURPLE      0x780F  ///< 123,   0, 123
-#define ILI9341_OLIVE       0x7BE0  ///< 123, 125,   0
-#define ILI9341_LIGHTGREY   0xC618  ///< 198, 195, 198
-#define ILI9341_DARKGREY    0x7BEF  ///< 123, 125, 123
-#define ILI9341_BLUE        0x001F  ///<   0,   0, 255
-#define ILI9341_GREEN       0x07E0  ///<   0, 255,   0
-#define ILI9341_CYAN        0x07FF  ///<   0, 255, 255
-#define ILI9341_RED         0xF800  ///< 255,   0,   0
-#define ILI9341_MAGENTA     0xF81F  ///< 255,   0, 255
-#define ILI9341_YELLOW      0xFFE0  ///< 255, 255,   0
-#define ILI9341_WHITE       0xFFFF  ///< 255, 255, 255
-#define ILI9341_ORANGE      0xFD20  ///< 255, 165,   0
-#define ILI9341_GREENYELLOW 0xAFE5  ///< 173, 255,  41
-#define ILI9341_PINK        0xFC18  ///< 255, 130, 198
+
 
 // For the Adafruit shield, these are the default.
 #define TFT_DC 9
 #define TFT_CS 10
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 // If using the breakout, change pins as desired
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 //24SPI, 28SPI
 UTFT myGLCD(ILI9341_S5P,MOSI,SCK,10,8,9);
@@ -48,7 +23,15 @@ UTFT myGLCD(ILI9341_S5P,MOSI,SCK,10,8,9);
  //touch panel (TCLK, TCS, TDIN, TDOUT, IRQ), XPT2046
 UTouch myTouch(6, 5, 4, 3, 2);
 
-long old_x=10, old_y=10;
+int16_t old_x = 10, 
+        old_y = 10, 
+        pad_topX = 10, 
+        pad_topY = 10, 
+        pad_bottomX = 229, 
+        pad_bottomY = 229;
+
+uint16_t pad_bg = ILI9341_BLACK, 
+         reticle_color = ILI9341_DARKGREEN;
 
 void setup()
 {
@@ -66,20 +49,19 @@ void setup()
   delay(10);
   Serial.println(FillScreen(ILI9341_BLACK));
   delay(10);
-
+  
 
   Serial.println("Klaarty");
   delay(30)
   myGLCD.clrScr();
-
-  myTouch.setPrecision(PREC_MEDIUM);
+  myTouch.setPrecision(PREC_HIGH);
+  
 
   draw_Main();
 }
 
-void loop()
-{
-  long x, y;
+void loop() {
+  int16_t x, y;
   //byte valueX;
   //byte valueY;  
   while (myTouch.dataAvailable() == true) 
@@ -105,80 +87,48 @@ void loop()
   }
 }
 
-unsigned long FillScreen(uint16_t color ) {
-  unsigned long start = micros();
-  tft.fillScreen(color);
-  yield();
-  return micros() - start;
-}
-
-unsigned long FillAxes() {
-  unsigned long start = micros();
-  int          w = tft.width(),
-               h = tft.height();
-
-  //tft.fillScreen(ILI9341_BLACK);
-  //yield();
-  tft.drawLine(0, h/2, w-1, h/2, ILI9341_DARKGREEN);
-  tft.drawLine(w/2, 0, w/2, h-1, ILI9341_DARKGREEN);
-
-  yield();
-  return micros() - start;
-}
-
-unsigned long FillPointer(long x, long y, uint16_t color) {
-  unsigned long start = micros();
-  tft.fillCircle(x, y, 3, color);
-  yield();
-  return micros() - start;  
-}
-
-void draw_Main() {
+unsigned long draw_Main() {
   unsigned long start = micros();
   tft.fillRect(pad_topX-2,pad_topY-2,pad_bottomX+2,pad_bottomY+2, pad_bg);
   tft.drawRect(pad_topX-2,pad_topY-2,pad_bottomX+2,pad_bottomY+2, reticle_color);  
-
-
-  tft.fillRect(239,8,312,58, ILI9341_BLACK); //button1 background 
-  tft.fillRect(239,68,312,116, ILI9341_BLACK); //button2 background 
-  tft.fillRect(239,124,312,174, ILI9341_BLACK); //button3 background 
-  tft.fillRect(239,182,312,232, ILI9341_BLACK); //button4 background 
+  tft.fillRect(239,8,312,58,pad_bg); //button1 background 
+  tft.fillRect(239,68,312,116,pad_bg); //button2 background 
+  tft.fillRect(239,124,312,174,pad_bg); //button3 background 
+  tft.fillRect(239,182,312,232,pad_bg); //button4 background 
   
   //Draw in green
-  tft.drawRect(239,8,312,58, ILI9341_DARKGREEN); //button1 rectangle
-  tft.drawRect(239,68,312,116, ILI9341_DARKGREEN); //button2 rectangle
-  tft.drawRect(239,124,312,174, ILI9341_DARKGREEN); //button3 rectangle
-  tft.drawRect(239,182,312,232, ILI9341_DARKGREEN); //button4rectangle
+  tft.drawRect(239,8,312,58,reticle_color); //button1 rectangle
+  tft.drawRect(239,68,312,116,reticle_color); //button2 rectangle
+  tft.drawRect(239,124,312,174,reticle_color); //button3 rectangle
+  tft.drawRect(239,182,312,232,reticle_color); //button4rectangle
 
-  tft.print("MIDI",240,22,); //button1 text
-  tft.print("X",240,78); //button2 text - line 1
-  tft.print("Y",240,134); //button3 text - line 1
-  tft.print("X+Y",240,192); //button4 text - line 1
-  
+  tft.println("MIDI",240,22); //button1 text
+  tft.println("X",240,78); //button2 text - line 1
+  tft.println("Y",240,134); //button3 text - line 1
+  tft.println("X+Y",240,192); //button4 text - line 1
+
   yield();
   return micros() - start;  
 }
 
-void draw_Pad(long x, long y)
-{
-      // we draw 3 three lines for x and three lines for y
-      // for better visibility
-      unsigned long start = micros();
+unsigned long draw_Pad(int16_t x, int16_t y) {
+      // we draw 3 three lines for x and three lines for y, for better visibility
+    unsigned long start = micros();
+    tft.drawLine(old_x-1,pad_topY,old_x-1,pad_bottomY, pad_bg); // clear old line x-1
+    tft.drawLine(old_x+1,pad_topY,old_x+1,pad_bottomY, pad_bg); // clear old line x+1
+    tft.drawLine(old_x,pad_topY,old_x,pad_bottomY, pad_bg);     // clear old line x
+    tft.drawLine(pad_topX,old_y-1,pad_bottomY,old_y-1, pad_bg); // clear old line y-1    
+    tft.drawLine(pad_topX,old_y+1,pad_bottomY,old_y+1, pad_bg); // clear old line y+1    
+    tft.drawLine(pad_topX,old_y,pad_bottomY,old_y, pad_bg);     // clear old line y
+
+    //Draw in green
+    tft.drawLine(x-1,pad_topY,x-1,pad_bottomY, reticle_color);         // draw new line x-1
+    tft.drawLine(x+1,pad_topY,x+1,pad_bottomY, reticle_color);         // draw new line x+1
+    tft.drawLine(x,pad_topY,x,pad_bottomY, reticle_color);             // draw new line x
+    tft.drawLine(pad_topX,y-1,pad_bottomX,y-1, reticle_color);         // draw new line1 y-1
+    tft.drawLine(pad_topX,y+1,pad_bottomX,y+1, reticle_color);         // draw new line2 y+1
+    tft.drawLine(pad_topX,y,pad_bottomX,y, reticle_color);             // draw new line3 y
       
-      tft.drawLine(old_x-1,pad_topY,old_x-1,pad_bottomY, ILI9341_BLACK); // clear old line x-1
-      tft.drawLine(old_x+1,pad_topY,old_x+1,pad_bottomY, ILI9341_BLACK); // clear old line x+1
-      tft.drawLine(old_x,pad_topY,old_x,pad_bottomY, ILI9341_BLACK);     // clear old line x
-      tft.drawLine(pad_topX,old_y-1,pad_bottomY,old_y-1, ILI9341_BLACK); // clear old line y-1    
-      tft.drawLine(pad_topX,old_y+1,pad_bottomY,old_y+1, ILI9341_BLACK); // clear old line y+1    
-      tft.drawLine(pad_topX,old_y,pad_bottomY,old_y, ILI9341_BLACK);     // clear old line y
-      //Draw in green
-      tft.drawLine(x-1,pad_topY,x-1,pad_bottomY, ILI9341_DARKGREEN);         // draw new line x-1
-      tft.drawLine(x+1,pad_topY,x+1,pad_bottomY, ILI9341_DARKGREEN);         // draw new line x+1
-      tft.drawLine(x,pad_topY,x,pad_bottomY, ILI9341_DARKGREEN);             // draw new line x
-      tft.drawLine(pad_topX,y-1,pad_bottomX,y-1, ILI9341_DARKGREEN);         // draw new line1 y-1
-      tft.drawLine(pad_topX,y+1,pad_bottomX,y+1, ILI9341_DARKGREEN);         // draw new line2 y+1
-      tft.drawLine(pad_topX,y,pad_bottomX,y, ILI9341_DARKGREEN);             // draw new line3 y
-      
-      yield();
-      return micros() - start;  
+    yield();
+    return micros() - start;  
 }
